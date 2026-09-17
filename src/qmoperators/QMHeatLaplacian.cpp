34d8587931d9cd6bb830874b94f0eed265488ca4
MarcusTL12 <marcuspaafjellet@gmail.com>
2026-09-17 13:22:12 +0200

    Return correct orbital

--- src/qmoperators/QMHeatLaplacian.cpp
+++ src/qmoperators/QMHeatLaplacian.cpp
@@ -34,7 +34,7 @@ extern mrcpp::MultiResolutionAnalysis<3> *mrchem::MRA;
 namespace mrchem {
 
 static double heat_kernel_coeffs_1[1] = {1.0};
-static double heat_kernel_coeffs_2[2] = {2.0, 0.5};
+static double heat_kernel_coeffs_2[2] = {2.0, -0.5};
 
 static double *heat_kernel_coeffs[2] = {heat_kernel_coeffs_1, heat_kernel_coeffs_2};
 static double constant_coeffs[2] = {-1.0, -1.5};
@@ -63,11 +63,11 @@ Orbital QMHeatLaplacian::apply(Orbital inp) {
     out_conv.defreal();
     out.defreal();
 
-    mrcpp::apply(prec, out.real(), *conv.get(), inp.real());
+    mrcpp::apply(prec, out_conv.real(), *conv.get(), inp.real());
 
     mrcpp::add(prec, out.real(), 1.0, out_conv.real(), constant_coeff, inp.real());
 
-    return inp;
+    return out;
 }
 
 } // namespace mrchem
